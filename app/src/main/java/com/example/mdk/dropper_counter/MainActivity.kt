85a88edf82a4c327c2b1234c42db483d4c6e64a1
maxdoctorkurt <maxdoctorkurt@mail.ru>
2018-03-13 22:28:06 +0300

    мелкие доработки 1

--- app/src/main/java/com/example/mdk/dropper_counter/MainActivity.kt
+++ app/src/main/java/com/example/mdk/dropper_counter/MainActivity.kt
@@ -15,11 +15,11 @@ class MainActivity : AppCompatActivity() {
 
     private var savedStationIP: String = ""
     private var savedDripsPerSecond: Int = 0
-//    private val IP_ADDRESS = Pattern.compile(
-//            "((25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\\.(25[0-5]|2[0-4]"
-//            + "[0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1]"
-//            + "[0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}"
-//            + "|[1-9][0-9]|[0-9]))")
+    private val IP_ADDRESS = Pattern.compile(
+            "((25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9])\\.(25[0-5]|2[0-4]"
+            + "[0-9]|[0-1][0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1]"
+            + "[0-9]{2}|[1-9][0-9]|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1][0-9]{2}"
+            + "|[1-9][0-9]|[0-9]))")
 
     private val client = OkHttpClient();
 
@@ -37,8 +37,12 @@ class MainActivity : AppCompatActivity() {
                 .enqueue(object : Callback {
                     override fun onFailure(call: Call?, e: IOException?) {
                         e?.printStackTrace();
-                        setProgress(false);
-                        showError("Не удалось совершить запрос. Проверьте подключение к точке доступа DripCounter (пароль - 00000000)")
+
+                        runOnUiThread {
+                            setProgress(false);
+                            showMessage("Не удалось совершить запрос. Проверьте подключение к точке доступа DripCounter (пароль - 00000000)")
+                        }
+
                     }
 
                     override fun onResponse(call: Call?, response: Response?) {
@@ -54,13 +58,16 @@ class MainActivity : AppCompatActivity() {
                             savedDripsPerSecond = speed.toInt()
                             savedStationIP = stationIP
 
-
-                            if(!isValidIp(savedStationIP))
-                                showError("Не удалось подключиться к требуемой точке - проверьте ввод и попробуйте еще раз")
-
                             runOnUiThread {
-                                etSensorIP.setText(savedStationIP.toString())
-                                etSensorValue.setText(savedDripsPerSecond.toString())
+
+                                if (!isValidIp(savedStationIP))
+                                    showMessage("Не удалось подключиться к требуемой точке - проверьте ввод и попробуйте еще раз")
+                                else {
+                                    etSensorIP.setText(savedStationIP.toString())
+                                    etSensorValue.setText(savedDripsPerSecond.toString())
+                                    showMessage("Начата настройка подключения... Повторите запрос черен несколько секунд для получения актуального IP адреса датчика")
+                                }
+
                                 setProgress(false);
                             }
                         }
@@ -74,13 +81,12 @@ class MainActivity : AppCompatActivity() {
 
         connect.setOnClickListener({
 
-            if(ssid.text.isEmpty()) {
-                showError("Пустое имя точки")
+            if (ssid.text.isEmpty()) {
+                showMessage("Пустое имя точки")
                 ssid.requestFocus()
                 return@setOnClickListener
-            }
-            else if(pass.text.length < 8) {
-                showError("Пароль должен иметь длину не менее 8 символов")
+            } else if (pass.text.length < 8) {
+                showMessage("Пароль должен иметь длину не менее 8 символов")
                 pass.requestFocus()
                 return@setOnClickListener
             }
@@ -94,11 +100,11 @@ class MainActivity : AppCompatActivity() {
 
 
     fun isValidIp(ip: String?): Boolean {
-        return Patterns.IP_ADDRESS.matcher(ip).matches();
+        return IP_ADDRESS.matcher(ip).matches();
     }
 
-    fun showError(e: String) {
-        Toast.makeText(this@MainActivity, e, Toast.LENGTH_LONG).show()
+    fun showMessage(msg: String) {
+        Toast.makeText(this@MainActivity, msg, Toast.LENGTH_LONG).show()
     }
 
 }
